e4643c882eed657c7fec8c814d9e866cd707700a
Matheus Mattana <matheusmattana@hotmail.com>
2020-09-28 00:31:04 -0300

    nav bar is 80% responsive now

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,34 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../AppContainer.css';
+import '../../hamburgers.css';
 
 import logoWhite from '../../assets/navBar/logoWhite.png';
 
 const NavBar = () => {
+  const [isNavActive, setIsNavActive] = useState(false);
+
   return (
-    <nav className="navBar-container">
-      <div className="navBar-items-container">
-        <div className="navBar-logo-container">
-          <img src={logoWhite} alt="web site logo" />
+    <>
+      <nav className="navBar-container">
+        <div className="navBar-items-container">
+          <div className="navBar-logo-container">
+            <img src={logoWhite} alt="web site logo" />
+          </div>
+          <nav className="stroke">
+            <ul className="navBar-navLinks-container">
+              <li>
+                <a href="/">What I do?</a>
+              </li>
+              <li>
+                <a href="/">About me</a>
+              </li>
+              <li>
+                <a href="/">My portifolio</a>
+              </li>
+            </ul>
+          </nav>
+          <div className="blankSpace">
+            <span className="language pt">PT</span>
+            <label className="switch">
+              <input type="checkbox" />
+              <span className="slider"></span>
+            </label>
+            <span className="language eng">EN</span>
+          </div>
+          <button
+            className={`hamburger hamburger--collapse ${
+              isNavActive ? 'is-active' : ''
+            }`}
+            id="hamburger"
+            type="button"
+            onClick={() => setIsNavActive(!isNavActive)}
+          >
+            <span className="hamburger-box">
+              <span className="hamburger-inner"></span>
+            </span>
+          </button>
         </div>
-        <nav className="stroke">
-          <ul className="navBar-navLinks-container">
-            <li>
-              <a href="/">What I do?</a>
-            </li>
-            <li>
-              <a href="/">About me</a>
-            </li>
-            <li>
-              <a href="/">My portifolio</a>
-            </li>
-          </ul>
-          <label class="switch">
-            <input type="checkbox" />
-            <span class="slider"></span>
-          </label>
+      </nav>
+      {isNavActive && (
+        <nav className="mobile-container">
+          <nav className="stroke">
+            <ul className="mobile-navLinks-container">
+              <li>
+                <a href="/">What I do?</a>
+              </li>
+              <li>
+                <a href="/">About me</a>
+              </li>
+              <li>
+                <a href="/">My portifolio</a>
+              </li>
+            </ul>
+          </nav>
+          <div className="mobile-blankSpace">
+            <span className="language pt">PT</span>
+            <label className="switch">
+              <input type="checkbox" />
+              <span className="slider"></span>
+            </label>
+            <span className="language eng">EN</span>
+          </div>
         </nav>
-      </div>
-    </nav>
+      )}
+    </>
   );
 };
 
